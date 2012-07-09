914bda0886f21db94b7630c54443739c4f251988
Jasper Palfree <well.caffeinated@gmail.com>
2012-07-08 22:10:15 -0400

    New build

--- library-build/js/require-config.js
+++ library-build/js/require-config.js
@@ -1,1 +1,1 @@
-require.config({jQuery:"1.7.2",waitSeconds:30,paths:{jquery:"libs/jquery"},map:{"*":{jquery:"modules/adapters/jquery","google/maps":"modules/adapters/google-maps"},"modules/adapters/jquery":{jquery:"jquery"}}})
+require.config({jQuery:"1.7.2",waitSeconds:30,paths:{jquery:"libs/jquery",stapes:"libs/stapes"},map:{"*":{jquery:"modules/adapters/jquery","google/maps":"modules/adapters/google-maps"},"plugins/json":{text:"plugins/text"},"modules/adapters/jquery":{jquery:"jquery"}}})
---
+++ library-build/js/templates/autocomplete.tpl
@@ -0,0 +1,6 @@
+{{! autocomplete results template }}
+<ul>
+	{{#results}}
+	<li class="item"><a href="#"><span class="sug">{{name}}</span></a></li>
+	{{/results}}
+</ul>
